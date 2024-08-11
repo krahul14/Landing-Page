be5b547ab8f142818f03bd785b26f62eb9bae65e
krahul14 <krahul52002@gmail.com>
2024-08-11 22:18:45 +0530

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { twMerge } from "tailwind-merge"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "AI Startup Landing Page",
   description: "A landing page for an AI startup created with Frontend Tribe",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={twMerge(inter.className, "bg-black text-white antialiased")}
+      >
+        {children}
+      </body>
     </html>
-  );
+  )
 }
